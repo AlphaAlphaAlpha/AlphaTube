e2950c4b291845a2934cb10587080f83cd4c2c9f
Alpha <56409408+AlphaAlphaAlpha@users.noreply.github.com>
2020-04-28 01:18:58 +0200

    h

---
+++ AlphaTube-master/README.md
@@ -0,0 +1,11 @@
+# AlphaTube
+How to install :
+
+cd AlphaTube
+
+bash install.sh
+
+
+How to use :
+
+python Alpha.py
---
+++ AlphaTube-master/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt -y full-upgrade -y
+sudo apt install git -y
+sudo apt install python -y
+sudo apt install python2 -y
+sudo apt install python3 -y
+sudo apt install python-pip -y
+sudo apt install python3-pip -y
+sudo pip install youtue-dl -y
+sudo apt-get install youtube-dl -y
+sudo pip install --upgrade youtube-dl -y
+sudo apt-get install libav-tools -y
+sudo apt install ffmpeg -y
